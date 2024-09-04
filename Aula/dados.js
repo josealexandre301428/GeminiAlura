e9edc97a40a0531a1f56fb90e53e4c4519c808d9
José Alexandre Filho <94934959+josealexandre301428@users.noreply.github.com>
2024-09-04 11:27:09 -0300

    FEAT: Aula 3

---
+++ Aula/dados.js
@@ -0,0 +1,85 @@
+const dados = [
+    {
+        titulo: 'O Poderoso Chefão',
+        ano: 1972,
+        diretor: 'Francis Ford Coppola',
+        genero: 'Crime',
+        sinopse: 'A saga da família Corleone, um dos clãs mafiosos mais poderosos dos Estados Unidos, que narra a ascensão de Michael Corleone ao poder.', 
+        link: "https://play.google.com/store/movies/details/O_Poderoso_Chef%C3%A3o?id=v41qEln2ShI.P&hl=de"
+    },
+    {
+        titulo: 'Um Sonho de Liberdade',
+        ano: 1994,
+        diretor: 'Frank Darabont',
+        genero: 'Drama',
+        sinopse: 'A história de Andy Dufresne, um homem injustamente condenado à prisão, que busca desesperadamente a redenção e a liberdade.',
+        link: "https://www.adorocinema.com/filmes/filme-11736/"
+    },
+    {
+        titulo: 'A Lista de Schindler',
+        ano: 1993,
+        diretor: 'Steven Spielberg',
+        genero: 'Drama Histórico',
+        sinopse: 'Um drama que narra a história de Oskar Schindler, um empresário alemão que salvou mais de mil judeus do Holocausto.',
+        link: "https://www.amazon.com/-/es/Poster-SCHINDLER-ORIGINAL-STEVEN-SPIELBERG/dp/B07NCBCVPG"
+    },
+    {
+        titulo: 'O Senhor dos Anéis: O Retorno do Rei',
+        ano: 2003,
+        diretor: 'Peter Jackson',
+        genero: 'Fantasia',
+        sinopse: 'A épica conclusão da trilogia, onde Frodo Bolseiro e seus companheiros enfrentam o Senhor das Trevas, Sauron, em uma batalha final para destruir o Um Anel.',
+        link: "https://www.elo7.com.br/big-poster-o-senhor-dos-aneis-o-retorno-do-rei-lo07-90x60-cm/dp/F28595"
+    },
+    {
+        titulo: 'Cidade de Deus',
+        ano: 2002,
+        diretor: 'Fernando Meirelles',
+        genero: 'Crime, Drama',
+        sinopse: 'Um retrato brutal e realista da vida nas favelas do Rio de Janeiro, que acompanha a história de vários jovens envolvidos no tráfico de drogas.',
+        link: "https://www.amazon.co.uk/Cidade-Painting-Posters-12x18inch-30x45cm/dp/B0CFRH4RJM"
+    },
+    {
+        titulo: 'O Cavaleiro das Trevas',
+        ano: 2008,
+        diretor: 'Christopher Nolan',
+        genero: 'Super-herói',
+        sinopse: 'Uma reinvenção do universo Batman, onde o herói se confronta com o Coringa, um vilão anárquico e psicopata.',
+        link: "https://www.elo7.com.br/quadro-batman-o-cavaleiro-das-trevas-2008-filme-nolan-a3/dp/1849CFD"
+    },
+    {
+        titulo: 'Pulp Fiction: Tempo de Violência',
+        ano: 1994,
+        diretor: 'Quentin Tarantino',
+        genero: 'Crime, Drama',
+        sinopse: 'Uma narrativa não linear que interliga várias histórias de crimes e violência, com diálogos icônicos e personagens excêntricos.',
+        link: "https://filmartgallery.com/products/pulp-fiction-4530"
+    },
+    {
+        titulo: 'O Pianista',
+        ano: 2002,
+        diretor: 'Roman Polanski',
+        genero: 'Drama Histórico',
+        sinopse: 'A história real de Władysław Szpilman, um pianista polonês que luta para sobreviver no gueto de Varsóvia durante a Segunda Guerra Mundial.',
+        link: "https://www.pinterest.com/pin/362047257515866789/"
+    },
+    {
+        titulo: 'Se7en: Os Sete Pecados Capitais',
+        ano: 1995,
+        diretor: 'David Fincher',
+        genero: 'Thriller',
+        sinopse: 'Dois detetives investigam uma série de assassinatos macabros, cada um relacionado a um dos sete pecados capitais.',
+        link: "https://www.amazon.com/Se7en-Sete-Pecados-Capitais/dp/B00005Z8QW"
+    },
+    {
+        titulo: 'O Clube da Luta',
+        ano: 1999,
+        diretor: 'David Fincher',
+        genero: 'Drama',
+        sinopse: 'A história de um homem insatisfeito com sua vida que encontra uma válvula de escape em um clube de luta clandestino.',
+        link: "https://www.amazon.com/Clube-Luta-1999-David-Fincher/dp/B00005Z8QW"
+    }
+];
+
+console.log(dados);
+
